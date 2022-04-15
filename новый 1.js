c23aba966358f8c37b3f321de7d1732a29d06271
jsfojdef <102535806+jsfojdef@users.noreply.github.com>
2022-04-15 20:16:57 +0300

    Add files via upload

---
+++ новый 1.js
@@ -0,0 +1,26 @@
+public boolean check(int a, int b) {
+    return (10 <= a + b) && (a + b <= 20);
+    }
+
+public static void isPositiveOrNegative(int a) {
+    if (a >= 0) {
+    System.out.println("+");
+    }
+        else {
+        System.out.println("-");
+        }
+
+
+public static boolean isNegative(int a) {
+        if (a >= 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+
+public static void printWordNTimes(String Word, int Times) {
+    for (int i = 1; i <= Times; i++) {
+    System.out.println("[" + i + "]" + "" + Word);
+    }
